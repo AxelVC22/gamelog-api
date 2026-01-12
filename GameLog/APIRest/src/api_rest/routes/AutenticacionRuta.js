c27374deb283a06bb7facd4e5dd8d2b42910d253
francoroguez <118698321+roguez26@users.noreply.github.com>
2026-01-11 18:40:50 -0600

    feat: add autentication to swagger

--- GameLog/APIRest/src/api_rest/routes/AutenticacionRuta.js
+++ GameLog/APIRest/src/api_rest/routes/AutenticacionRuta.js
@@ -1,19 +1,58 @@
 import { Router } from "express";
 import { AutenticacionControlador } from "../controllers/AutenticacionControlador.js";
 
-export const CrearRutaAutenticacion = () =>
-{
-    /**
+export const CrearRutaAutenticacion = () => {
+
+     /**
      * @swagger
      * tags:
      *  name: Autenticacion
-     *  description: Rutas del inicio de sesion
+     *  description: Gestión de las renovación de tokens
      */
-
-    const AutenticacionEnrutador = Router();
+     const AutenticacionEnrutador = Router();
     const ControladorAutenticacionEnrutador = new AutenticacionControlador();
 
-        AutenticacionEnrutador.post('/',ControladorAutenticacionEnrutador.Autenticar);
+    /**
+ * @swagger
+ * /gamelog/autenticacion:
+ *   post:
+ *     summary: Renueva los tokens de autenticación usando un refresh token válido
+ *     tags: [Autenticacion]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - refreshToken
+ *             properties:
+ *               refreshToken:
+ *                 type: string
+ *                 description: Token de renovación emitido previamente
+ *     responses:
+ *       200:
+ *         description: Tokens renovados correctamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 accessToken:
+ *                   type: string
+ *                   description: Nuevo token de acceso
+ *                 refreshToken:
+ *                   type: string
+ *                   description: Nuevo refresh token (rotado)
+ *       401:
+ *         description: Refresh token inválido o expirado
+ *       403:
+ *         description: Refresh token revocado
+ */
+
+
+
+    AutenticacionEnrutador.post('/', ControladorAutenticacionEnrutador.Autenticar);
 
     return AutenticacionEnrutador;
 
